681fccc1fd3b4c0c5aacc713cccc20ed16a19a05
Jordan Nicols <nicolsj99@gmail.com>
2020-02-25 01:25:34 -0700

    Add project files.

---
+++ AjaxActivity/Controllers/CustomerController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AjaxActivity.Models;
+
+namespace AjaxActivity.Controllers
+{
+    public class CustomerController : Controller
+    {
+
+        List<CustomerModel> customers;
+
+        public CustomerController()
+        {
+            customers = new List<CustomerModel>();
+            customers.Add(new CustomerModel(0, "Sherry", 37));
+            customers.Add(new CustomerModel(1, "Jordan", 20));
+            customers.Add(new CustomerModel(2, "Shad", 21));
+            customers.Add(new CustomerModel(3, "Cyrus", 21));
+            customers.Add(new CustomerModel(4, "Shaylah", 20));
+            customers.Add(new CustomerModel(5, "Brandon", 24));
+            customers.Add(new CustomerModel(6, "Tammy", 45));
+        }//end default constructor
+
+        // GET: Customer
+        public ActionResult Index()
+        {
+            Tuple<List<CustomerModel>, CustomerModel> tuple;
+            tuple = new Tuple<List<CustomerModel>, CustomerModel>(customers, customers[3]);
+
+            return View("Customer", tuple);
+        }//end index
+
+        [HttpPost]
+        public ActionResult OnSelectCustomer(string CustomerNumber)
+        {
+            int customerID = Int32.Parse(CustomerNumber);
+            Tuple<List<CustomerModel>, CustomerModel> tuple;
+            tuple = new Tuple<List<CustomerModel>, CustomerModel>(customers, customers[customerID]);
+
+            return PartialView("~/Views/Shared/_CustomerDetails.cshtml", customers[customerID]);
+        }//end OnSelectCustomer
+    }//end controller class
+}//end namespace
---
+++ AjaxActivity/Models/CustomerModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AjaxActivity.Models
+{
+    public class CustomerModel
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public CustomerModel(int iD, string name, int age)
+        {
+            ID = iD;
+            Name = name;
+            Age = age;
+        }//end non-default constructor
+    }//end class
+}//end namespace
